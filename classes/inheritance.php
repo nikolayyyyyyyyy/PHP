bd43c2343995282c52f10e680c6a8516f1cb3a38
Николай <149511212+nikolayyyyyyyyy@users.noreply.github.com>
2025-04-07 16:12:56 +0300

    Create inheritance.php

---
+++ classes/inheritance.php
@@ -0,0 +1,46 @@
+<?php
+abstract class Person
+{
+    private $first_name;
+    private $last_name;
+
+    function __construct($first, $last)
+    {
+        $this->first_name = $first;
+        $this->last_name = $last;
+    }
+
+    function show_person()
+    {
+        return "My name is, " . $this->first_name . " " . $this->last_name . "!";
+    }
+}
+
+class Programmer extends Person
+{
+    private $langs;
+
+    function __construct($first, $last)
+    {
+        parent::__construct($first, $last);
+        $this->langs = [];
+    }
+
+    function set_lang($lang)
+    {
+        array_push($this->langs, $lang);
+    }
+
+    function describe()
+    {
+        echo parent::show_person() . PHP_EOL;
+        echo "I know also PHP!" . PHP_EOL;
+        print_r($this->langs);
+    }
+}
+
+
+$programmer = new Programmer('Nikolay', 'Nikolaev');
+$programmer->set_lang('Java');
+$programmer->set_lang('MySql');
+$programmer->describe();
