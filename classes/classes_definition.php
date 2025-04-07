6b8c8c9550c4d996705b41c23cd17f9a6b7f4fd3
Николай <149511212+nikolayyyyyyyyy@users.noreply.github.com>
2025-04-07 21:19:51 +0300

    Update and rename ex3.php to classes_definition.php

--- classes/classes_definition.php
+++ classes/classes_definition.php
@@ -1,9 +1,9 @@
 <?php
 class Article
 {
-    public $title;
-    public $author;
-    public $description;
+    private $title;
+    private $author;
+    private $description;
 
     function __construct($title, $author, $description)
     {
@@ -14,15 +14,15 @@ class Article
 
     function show_article()
     {
-        return "Title: " . $this->title . " Author: " . $this->author . " Description: " . $this->description;
+        return "Title: " . $this->title . ", Author: " . $this->author . ", Description: " . $this->description;
     }
 }
 
 class Person
 {
-    public $first_name;
-    public $last_name;
-    public $email;
+    private $first_name;
+    private $last_name;
+    private $email;
 
     function __construct($first, $last, $email)
     {
@@ -33,6 +33,6 @@ class Person
 
     function show_person()
     {
-        return "First Name: " . $this->first_name . " Last Name: " . $this->last_name . " Email: " . $this->email;
+        return "First Name: " . $this->first_name . ", Last Name: " . $this->last_name . ", Email: " . $this->email;
     }
 }
