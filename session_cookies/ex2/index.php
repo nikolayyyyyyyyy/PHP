029583bab5eef17ac113f1488ad420f94c08bad4
Николай <149511212+nikolayyyyyyyyy@users.noreply.github.com>
2025-04-14 20:25:11 +0300

    Create index.php

---
+++ session_cookies/ex2/index.php
@@ -0,0 +1,16 @@
+<?php setcookie('username', 'Alex Porter', time() + 3600, '/'); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web browser</title>
+</head>
+
+<body>
+    <a href="welcome.php">Next page - welcome</a>
+    <br>
+</body>
+
+</html>
