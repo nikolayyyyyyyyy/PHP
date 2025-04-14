e44e87280f8b43dc7b20c65996d7183d53d3bf49
Николай <149511212+nikolayyyyyyyyy@users.noreply.github.com>
2025-04-14 20:24:02 +0300

    Create quiz1.php

---
+++ session_cookies/quiz_game/quiz1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web</title>
+</head>
+
+<body>
+    <form action="quiz2.php" method="post">
+        <h3>Question 1: What is the capital of France?</h3>
+        <input type="radio" name="answer1" value="Paris"> Paris
+        <br>
+        <input type="radio" name="answer1" value="London"> London
+        <br>
+        <input type="radio" name="answer1" value="Berlin"> Berlin
+        <br>
+        <input type="submit" value="Next">
+    </form>
+</body>
+
+</html>
