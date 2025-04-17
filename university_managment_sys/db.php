db01d9598ac5cdd107c223c85da51fbe38f59df5
Николай <149511212+nikolayyyyyyyyy@users.noreply.github.com>
2025-04-17 22:39:54 +0300

    Update db.php

--- university_managment_sys/db.php
+++ university_managment_sys/db.php
@@ -7,76 +7,76 @@ $db = new PDO(
 );
 
 $db->exec("CREATE TABLE IF NOT EXISTS specialties (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(20) UNIQUE NOT NULL
+                        id INT PRIMARY KEY AUTO_INCREMENT,
+                        `name` VARCHAR(20) UNIQUE NOT NULL
 )");
 
 $db->exec("CREATE TABLE IF NOT EXISTS students (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    faculty_number INT UNIQUE NOT NULL,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    `address` VARCHAR(150) UNIQUE NOT NULL,
-    specialty_id INT NOT NULL,
-    cource_id INT NOT NULL,
-    FOREIGN KEY (specialty_id)
-        REFERENCES specialties (id)
+                        id INT PRIMARY KEY AUTO_INCREMENT,
+                        faculty_number INT UNIQUE NOT NULL,
+                        first_name VARCHAR(50) NOT NULL,
+                        last_name VARCHAR(50) NOT NULL,
+                        email VARCHAR(100) UNIQUE NOT NULL,
+                        `address` VARCHAR(150) UNIQUE NOT NULL,
+                        specialty_id INT NOT NULL,
+                        cource_id INT NOT NULL,
+                        FOREIGN KEY (specialty_id)
+                            REFERENCES specialties (id)
 )");
 
 $db->exec("CREATE TABLE IF NOT EXISTS titles (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(10) UNIQUE NOT NULL
+                        id INT PRIMARY KEY AUTO_INCREMENT,
+                        `name` VARCHAR(10) UNIQUE NOT NULL
 )");
 
 $db->exec("CREATE TABLE IF NOT EXISTS departments (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(50) UNIQUE NOT NULL
+                        id INT PRIMARY KEY AUTO_INCREMENT,
+                        `name` VARCHAR(50) UNIQUE NOT NULL
 )");
 
 $db->exec("CREATE TABLE IF NOT EXISTS teachers (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    telephone_number CHAR(10) UNIQUE NOT NULL,
-    email VARCHAR(50) UNIQUE NOT NULL,
-    title_id INT NOT NULL,
-    department_id INT NOT NULL,
-    CONSTRAINT ck_telephone_teacher CHECK (LENGTH(telephone_number) = 10),
-    FOREIGN KEY (title_id)
-        REFERENCES titles (id),
-    FOREIGN KEY (department_id)
-        REFERENCES departments (id)
+                        id INT PRIMARY KEY AUTO_INCREMENT,
+                        telephone_number CHAR(10) UNIQUE NOT NULL,
+                        email VARCHAR(50) UNIQUE NOT NULL,
+                        title_id INT NOT NULL,
+                        department_id INT NOT NULL,
+                        CONSTRAINT ck_telephone_teacher CHECK (LENGTH(telephone_number) = 10),
+                        FOREIGN KEY (title_id)
+                            REFERENCES titles (id),
+                        FOREIGN KEY (department_id)
+                            REFERENCES departments (id)
 )");
 
 $db->exec("CREATE TABLE IF NOT EXISTS disciplines (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(50) UNIQUE NOT NULL,
-    semester VARCHAR(10) NOT NULL,
-    credits INT NOT NULL,
-    teacher_id INT NOT NULL,
-    CONSTRAINT ck_semester CHECK (semester IN ('Зимен' , 'Летен')),
-    FOREIGN KEY (teacher_id) REFERENCES teachers(id)
+                        id INT PRIMARY KEY AUTO_INCREMENT,
+                        `name` VARCHAR(50) UNIQUE NOT NULL,
+                        semester VARCHAR(10) NOT NULL,
+                        credits INT NOT NULL,
+                        teacher_id INT NOT NULL,
+                        CONSTRAINT ck_semester CHECK (semester IN ('Зимен' , 'Летен')),
+                        FOREIGN KEY (teacher_id) REFERENCES teachers(id)
 )");
 
 $db->exec("CREATE TABLE IF NOT EXISTS grades (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    grade DECIMAL(3 , 2 ) NOT NULL,
-    faculty_number INT NOT NULL,
-    discipline_id INT NOT NULL,
-    `date` DATE NOT NULL
+                        id INT PRIMARY KEY AUTO_INCREMENT,
+                        grade DECIMAL(3 , 2 ) NOT NULL,
+                        faculty_number INT NOT NULL,
+                        discipline_id INT NOT NULL,
+                        `date` DATE NOT NULL
 )");
 
 $db->exec("CREATE TABLE IF NOT EXISTS roles (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(10) UNIQUE NOT NULL
+                        id INT PRIMARY KEY AUTO_INCREMENT,
+                        `name` VARCHAR(10) UNIQUE NOT NULL
 )");
 
 $db->exec("CREATE TABLE IF NOT EXISTS users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    f_number INT UNIQUE NOT NULL,
-    `password` VARCHAR(100) NOT NULL,
-    role_id INT NOT NULL,
-    FOREIGN KEY (role_id)
-        REFERENCES roles (id)
+                        id INT PRIMARY KEY AUTO_INCREMENT,
+                        f_number INT UNIQUE NOT NULL,
+                        `password` VARCHAR(100) NOT NULL,
+                        role_id INT NOT NULL,
+                        FOREIGN KEY (role_id)
+                            REFERENCES roles (id)
 )");
 
 $db->exec("INSERT INTO roles (`name`) VALUES ('ADMIN')");
